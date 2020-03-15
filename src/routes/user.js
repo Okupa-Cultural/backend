abe6ea1f86a8ab3cbe07c068f1cbb28bff36b328
hidanscript <hidanshira2@gmail.com>
2020-03-15 04:13:26 -0300

    Mejoradas validaciones de login, signup y logout

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,33 +3,51 @@ const router = Router();
 const User = require('../models/user');
 
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res, next) => {
     const { username, password, email } = req.body;
+    let alreadyExists = false;
 
-    if(username && password) {
-        const newUser = new User({ ...req.body });
-        newUser.password = newUser.encryptPassword(newUser.password);
-        newUser.save();
-        res.json(newUser);
-    } else {
-        res.send("Datos incorrectos!");
+    await User.findOne({ username }, function (err, user) {
+        if(err) return handleError(err);
+
+        if(user) {
+            alreadyExists = true;
+            res.send("username already in use");
+            next();
+        }
+    });
+
+    if(!alreadyExists) {
+        if(username && password && email) {
+            const newUser = new User({ ...req.body });
+            newUser.password = newUser.encryptPassword(newUser.password);
+            newUser.save();
+            res.json(newUser);
+        } else {
+            res.send("Datos incorrectos!");
+        }
     }
+
 });
 
 router.post('/login', async ( req, res ) => {
     const { username , password } = req.body;
     let theUser;
 
-    await User.findOne({ username: username } , function (err, user) {
-        if(err) return handleError(err);
+    if(username && password) {
+        await User.findOne({ username: username } , function (err, user) {
+            if(err) return handleError(err);
 
-        if(user.validatePassword(password)) {
-            theUser = user;
-            let token = theUser.generateToken();
-            theUser.token = token;
-            theUser.save();
-        }
-    });
+            if(user) {
+                if(user.validatePassword(password)) {
+                    theUser = user;
+                    let token = theUser.generateToken();
+                    theUser.token = token;
+                    theUser.save();
+                }
+            }
+        });
+    }
 
     if(theUser) {
         res.json(theUser);
@@ -41,13 +59,19 @@ router.post('/login', async ( req, res ) => {
 router.post('/logout', async ( req, res ) => {
     const { username , token } = req.body;
 
-    await User.findOne({ username : username , token : token }, 'token', function (err, user) {
-        if(err) return handleError(err);
+    if(username && token) {
+        await User.findOne({ username : username , token : token }, 'token', function (err, user) {
+            if(err) return handleError(err);
 
-        user.deleteToken();
-        user.save();
-        res.json(user);
-    });
+            if(user) {
+                user.deleteToken();
+                user.save();
+                res.json(user);
+            } else {
+                res.send("invalid user or token provided");
+            }
+        });
+    }
 });
 
 module.exports = router;
